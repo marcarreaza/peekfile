c04faa2adf8df36f1d14a944e979e5ec97a0d621
marcarreaza <marc.arba02@gmail.com>
2024-10-22 16:45:52 +0200

    first version

---
+++ peek.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Use command substitution to store the first and last three lines in variables
+first_three_lines=$(head -n 3 "$1")
+last_three_lines=$(tail -n 3 "$1")
+
+# Print the first three lines followed by the last three lines
+echo "$first_three_lines"
+echo "..."
+echo "$last_three_lines"
+
